ef44d7b87f9813f2d6d40ed6a2e4052c1be8e794
Ravi <ravigupta9363@gmail.com>
2017-08-22 22:28:43 +0530

    File Upload Done.

---
+++ common/settings/fileUpload/credentials/imagerConfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+    variants: {
+        items: {
+            resize: {
+                thumb: "300x300",
+                original: "100%",
+                medium: "80%",
+                small: "400"
+            }
+        }
+    },
+
+    storage: {
+        S3: {
+            key: 'AKIAJDJMX5CFG443RZBA',
+            secret: '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',
+            bucket: 'autoboxsnaphy',
+            storageClass: 'REDUCED_REDUNDANCY',
+            secure: false, // (optional) if your BUCKET_NAME contains dot(s), set this to false. Default is `true`
+            cdn: 'CDN_URL' // (optional) if you want to use Amazon cloudfront cdn, enter the cdn url here
+        }
+    },
+    debug: true
+};
