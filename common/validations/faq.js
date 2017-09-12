9ffe1c7d58ce6ab426ea6c0fdaf113371ff66cd6
nikita <nikita@snaphy.com>
2017-09-12 15:41:11 +0530

    Capitalize problem fixed

--- common/validations/faq.js
+++ common/validations/faq.js
@@ -26,23 +26,23 @@ module.exports = function(Faq, server, hepler) {
 
         if(!instance.question){
             return next(new Error("Question is required"));
-        } else{
+        } /*else{
             instance.question = _.capitalize(trim(instance.question));
             const check = isLength(instance.question, {min:3, max:500});
             if(!check){
                 return next(new Error("Question Length should be between 3 and 500"));
             }
-        }
+        }*/
 
         if(!instance.answer){
             return next(new Error("Answer is required"));
-        } else{
+        } /*else{
             instance.answer = _.capitalize(instance.answer, {min:3, max:1000});
             const check = isLength(instance.question, {min:3, max:1000});
             if(!check){
                 return next(new Error("Answer Length should be between 3 and 1000"));
             }
-        }
+        }*/
 
         next();
 
