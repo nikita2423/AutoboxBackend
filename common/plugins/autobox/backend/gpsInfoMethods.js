652df2352631e4744337169424e6c591ac5b744f
Your Name <you@example.com>
2017-10-08 15:33:21 +0530

    school panel

--- common/plugins/autobox/backend/gpsInfoMethods.js
+++ common/plugins/autobox/backend/gpsInfoMethods.js
@@ -283,7 +283,18 @@ module.exports = function( server, databaseObj, helper, packageObj) {
                                      });
                                      if(count>0){
                                          //upsert
-                                         return gpsTrackerInfoInstance.updateAttribute("gpsPassword", gpsTrackerInfoInstance.gpsPassword);
+                                         return GpsTrackerInfo.upsert({
+                                             deviceIMEI : gpsTrackerInfoInstance.deviceIMEI,
+                                             gpsPassword : gpsTrackerInfoInstance.gpsPassword,
+                                             registrationNumber : gpsTrackerInfoObj.registrationNumber,
+                                             serialNumber : gpsTrackerInfoObj.serialNumber,
+                                             modelName : gpsTrackerInfoObj.modelName,
+                                             status : gpsTrackerInfoInstance.status,
+                                             added: gpsTrackerInfoInstance.added,
+                                             updated: gpsTrackerInfoInstance.updated,
+                                             customerId: gpsTrackerInfoInstance.customerId,
+                                             id : gpsTrackerInfoInstance.id
+                                         });
                                      } else{
                                          //create
                                          return GpsTrackerInfo.create({
